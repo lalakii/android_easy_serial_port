befd25daa4a2eeadc9237259c331d5f538935e59
lalakii <sa@iamverylovely.com>
2024-02-26 01:36:55 +0800

    build_clean

---
+++ library/src/main/java/cn/lalaki/SerialPort.kt
@@ -0,0 +1,46 @@
+package cn.lalaki
+
+import android.os.ParcelFileDescriptor
+import java.io.File
+
+class SerialPort(path: String, speed: Int, private val mDataCallBack: DataCallback?) {
+    private var mFD = -1
+
+    init {
+        System.loadLibrary("comm1")
+        try {
+            val pfd = ParcelFileDescriptor.open(File(path), ParcelFileDescriptor.MODE_READ_WRITE)
+            mFD = pfd.detachFd()
+            if (mFD == -1) throw Exception()
+            open(mFD, speed)
+            pfd.close()
+        } catch (_: Exception) {
+            mDataCallBack?.onData(byteArrayOf(0xE))
+        }
+    }
+
+    private fun onNativeData(data: ByteArray) {
+        if (mFD != -1) mDataCallBack?.onData(data)
+    }
+
+    fun write(data: ByteArray) {
+        val fd = mFD
+        if (fd != -1) write(fd, data)
+    }
+
+    fun close() {
+        val fd = mFD
+        if (fd != -1) {
+            close(fd)
+            mFD = -1
+        }
+    }
+
+    interface DataCallback {
+        fun onData(data: ByteArray?)
+    }
+
+    private external fun open(fd: Int, speed: Int)
+    private external fun write(fd: Int, data: ByteArray)
+    private external fun close(fd: Int)
+}
